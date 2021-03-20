b0d8fb6f041be72ce8ded268521a54176acd4a42
att10 <atthai99@example.com>
2021-03-20 17:54:00 -0400

    added color and shape distributions

--- randomart.py
+++ randomart.py
@@ -5,16 +5,17 @@ from PIL import ImageFilter
 import numpy as np
 import random
 
+seed = 2048
+random.seed(seed)
 
 side_length = 56
 
-square = Image.open("square.png")
-top_left = Image.open("corner2.png")
+square = Image.open("src_img/square.png")
+top_left = Image.open("src_img/corner2.png")
 bot_left = top_left.rotate(90)
 bot_right = bot_left.rotate(90)
 top_right = bot_right.rotate(90)
 
-original = Image.open("art.png")
 output = Image.new("RGBA", (10*56, 10*56))
 output_px = np.array(output)
 
@@ -22,11 +23,18 @@ red = [237, 28, 36, 255]
 black = [0, 0, 0, 255]
 
 shapes = [square, top_left, top_right, bot_right, bot_left]
+
+# yellow, light blue, black, dark green, red orange, dark blue, coral
+# brown, magenta
+# orange yellow, white, gray, pink, light pink, teal, light teal
 colors = [[223, 226, 75, 255], [144, 176, 227, 255], [0, 0, 0, 255], 
         [28, 52, 38, 255],[215, 59, 21, 255],[36, 36, 72, 255], 
         [255, 137, 123, 255], [178, 86, 39, 255], [124, 26, 69, 255], 
         [254, 182, 39, 255], [255, 255, 255, 255], [183, 183, 183, 255], 
         [246,202,189, 255], [236,217,210, 255], [103,201,186, 255], [163,223,198,255]]
+
+color_weights = [7, 7, 35, 10, 6, 9, 5, 3, 3, 2, 30, 10, 6, 4, 6, 5]
+shape_weights = [33, 16, 17, 17, 17]
 
 # Williams Identity Colors and Color Distribution
 w_colors = [[80, 0, 130, 255], [255, 190, 10, 255], black, [255, 255, 255, 255],
@@ -37,10 +45,10 @@ w_weights = [22, 22, 7, 22, 7, 7, 7, 7]
 
 
 def get_shape():
-    return random.choice(shapes)
+    return random.choices(shapes,weights=shape_weights, k=1)[0]
 
 def get_color():
-    return random.choice(colors)
+    return random.choices(colors,weights=color_weights, k=1)[0]
 
 def get_w_color():
     return random.choices(w_colors, weights=w_weights, k=1)[0]
@@ -62,7 +70,8 @@ def make_art(grid_size):
                         
 def main():
     make_art(10)
-    Image.fromarray(output_px).show()
+    im = Image.fromarray(output_px)
+    im.save(str(seed) + ".png", "PNG")
     #Image.fromarray(output_px).filter(ImageFilter.SMOOTH).show()
     
 
